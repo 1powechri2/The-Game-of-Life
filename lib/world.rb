41d8a852491f2e596378c5bc4efcc3a631f3d348
Chris Powell <chris.ian.powell@gmail.com>
2018-05-07 07:03:00 -0600

    added comment about Matrix library

--- lib/world.rb
+++ lib/world.rb
@@ -12,3 +12,7 @@
  # world index 2 = [1, 1, 0]
  # this creates our 2 dimensional matrix where we can
  # compare neighbors for the next generation.
+ # I found a ruby library that we can use to do this easily
+ # called Martrix. Here is an example in pry:
+ # pry(main)> Matrix.build(4,4){|row, col| rand(2)}
+ # =>Matrix[[1, 0, 1, 1], [1, 1, 0, 1], [1, 0, 1, 1], [1, 1, 1, 0]]
